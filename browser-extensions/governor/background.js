68e2bdaf16c5d2829651cee3b55ee6b8c2618f0f
Mike Fine <mike_fine@cable.comcast.com>
2022-02-22 15:16:10 -0800

    Clean up post function

--- browser-extensions/governor/background.js
+++ browser-extensions/governor/background.js
@@ -27,18 +27,17 @@ function setPortNumber(pn) { g_portNumber = pn; }
 
 
 async function _postData(url = '', data = {}) {
-  // Default options are marked with *
   const response = await fetch(url, {
     method: 'POST',
-    mode: 'cors',                         // no-cors, *cors, same-origin
-    cache: 'no-cache',                    // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin',           // include, *same-origin, omit
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
     headers: {
-      'Content-Type': 'application/json'  // 'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/json'
     },
-    redirect: 'follow',                   // manual, *follow, error
-    referrerPolicy: 'no-referrer',        // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data)             // body data type must match "Content-Type" header
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data)
   });
   return response.json();
 }
