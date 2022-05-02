8c25197bad5344095e0fcc72c8d5324fe4361b9d
Thederahn, Noah <noah.thederahn@accenture.com>
2022-05-02 12:55:08 -0400

    Created Storage Structs

---
+++ server/src/sessionOBJ.mjs
@@ -0,0 +1,33 @@
+
+class FireboltCall {
+    constructor(methodCall, params) {
+        this.methodCall = methodCall;
+        this.params = params;
+        this.timestamp = Date.now();
+    }
+}
+
+class Session{
+    #sessionStart;
+    #sessionEnd;
+    
+    constructor(){
+        this.calls = [];
+        this.#sessionStart = Date.now();
+    }
+
+    exportSession(){
+        this.#sessionEnd = Date.now();
+        // Save Session data to JSON file
+        const sessionData = {
+            sessionStart: this.#sessionStart,
+            sessionEnd: this.#sessionEnd,
+            calls: this.calls
+        };
+        const sessionDataJson = JSON.stringify(sessionData);
+        const sessionDataFile = `FireboltCalls_${this.#sessionStart}.json`;
+        fs.writeFileSync(sessionDataFile, sessionDataJson);
+    }
+}
+
+export {Session, FireboltCall};
