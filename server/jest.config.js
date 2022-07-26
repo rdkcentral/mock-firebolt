ba2607fa0c0b3ef8594b8eb18c566a6f80c486f0
Vikas Warbhe <amit.vikas.warbhe@accenture.com>
2022-07-26 14:38:55 +0530

    github actions for unit test cases

--- server/jest.config.js
+++ server/jest.config.js
@@ -21,6 +21,7 @@ module.exports = {
   moduleFileExtensions: ["js", "jsx", "mjs"],
 
   collectCoverage: true,
+  coverageThreshold: {global: {lines: 100,},},
   collectCoverageFrom: [
     './src/**/*.mjs',
     '!**<rootDir>/server/node_modules/**'
