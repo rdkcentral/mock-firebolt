f6f8a6b682725cbe2ee5f72984befff2c7c122e9
Vikas Warbhe <amit.vikas.warbhe@accenture.com>
2022-08-12 16:18:49 +0530

    updated dotConfig test file as per review comment

--- server/test/suite/dotConfig.test.mjs
+++ server/test/suite/dotConfig.test.mjs
@@ -23,31 +23,6 @@
 import { dotConfig } from "../../src/dotConfig.mjs";
 
 test(`dotConfig works properly`, () => {
-  const expectedResult = {
-    supportedSdks: [
-      {
-        cliFlag: null,
-        cliShortFlag: null,
-        enabled: true,
-        fileName: "firebolt-open-rpc.json",
-        name: "core",
-      },
-      {
-        cliFlag: "manage",
-        cliShortFlag: "m",
-        enabled: false,
-        fileName: "firebolt-manage-open-rpc.json",
-        name: "manage",
-      },
-      {
-        cliFlag: "discovery",
-        cliShortFlag: "d",
-        enabled: false,
-        fileName: "firebolt-discovery-open-rpc.json",
-        name: "discovery",
-      },
-    ],
-    validateMethodOverrides: true,
-  };
-  expect(dotConfig).toEqual(expectedResult);
+  const result = JSON.parse(JSON.stringify(dotConfig));
+  expect(result).not.toBeUndefined();
 });
