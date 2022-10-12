2b4a15aefa145ae024562b3c7369a5adf4ff97b2
kschrief <kschriefer91@gmail.com>
2022-10-12 16:28:32 -0400

    Further examples

--- functional/tests/sample.test.js
+++ functional/tests/sample.test.js
@@ -2,7 +2,7 @@ import {expect, jest, test} from '@jest/globals';
 import utilities from './utilities.mjs'
 
 beforeEach(async () => {
-  //Will throw rejected promise if failure
+  //Stop Mock Firebolt
   console.log(await utilities.mfState(false))
 })
 
@@ -15,3 +15,21 @@ test(`MF Startup/Health Check`, async () => {
   expect(healthCheckResponse).toBeDefined()
   expect(healthCheckResponse.status).toBe(200)
 });
+
+test('MF Startup -> Set override response -> Send Firebolt Command', async () => {
+
+  //Start Mock Firebolt
+  await utilities.mfState(true)
+
+  //Tell the CLI to set an override for "account.id" method. Set response to "111"
+  //await utilities.callMfCli('--method account.id --result "111"')
+  //Validate the response
+
+  //Send an "account.id" firebolt command
+  //let response = utilities.fireboltCommand('{"method": "account.id"}')
+
+  //Validate that "response" contains the override we set previously
+  //assert(response).contains('111')
+
+
+})
