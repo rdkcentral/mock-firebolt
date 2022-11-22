7b5c357441858758b5ee161f95eb641598aec88e
Chakraborty <s.ei.chakraborty@accenture.com>
2022-11-22 13:18:46 +0530

    worked on review comments

--- functional/tests/core.test.mjs
+++ functional/tests/core.test.mjs
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2021 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 "use strict";
 
 import { expect, jest, test, afterAll, beforeAll } from "@jest/globals";
@@ -6,12 +24,14 @@ import * as utilities from "./utilities.mjs";
 jest.setTimeout(20000);
 
 beforeAll(async () => {
-  console.log(await utilities.mfState(true));
+  const response = await utilities.mfState(true);
+  expect(response).toBe("MF started successfully");
 });
 
 afterAll(async () => {
   //Stop Mock Firebolt
-  console.log(await utilities.killPort(9998));
+  const response = await utilities.killPort(9998);
+  expect(response).toBe("Port Killed");
 });
 
 // Health check on default port with HTTP
