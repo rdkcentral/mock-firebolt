848bdaa9ff1c91278b5bcfc0a1d5a65711d17147
Kandasamy, Divya <kdivya153@gmail.com>
2023-05-03 14:20:22 -0400

    added

--- functional/tests/core-manage-jsonrpc.test.mjs
+++ functional/tests/core-manage-jsonrpc.test.mjs
@@ -21,7 +21,7 @@
 import { expect, jest, test, afterAll, beforeAll } from "@jest/globals";
 import * as utilities from "./utilities.mjs";
 
-jest.setTimeout(50000);
+jest.setTimeout(20000);
 
 beforeAll(async () => {
   const response = await utilities.mfState(
